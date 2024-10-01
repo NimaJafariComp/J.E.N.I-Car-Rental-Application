858a4d041179b6a08d4decf7a6c26a05a0314eff
ElijahSagaranCSUN <elijah.sagaran.978@my.csun.edu>
2024-10-01 14:37:58 -0700

    Create test_database_class.py
    testing main for database_class

---
+++ back_end/database_class/main_database_class.py
@@ -0,0 +1,11 @@
+import class_database
+
+my_username = input("Enter your MySQL username: ")
+my_password = input("Enter your MySQL password: ")
+
+database_object = class_database.test(my_username, my_password)
+# adding_car.add_car('JNKCV51EX4M613956', 1000, 25, 60.0, 1, '9YNH255')
+
+carID = database_object.get_car_id('JNKCV51EX4M613956', '9YNH255')
+
+database_object.get_car_info(carID)
