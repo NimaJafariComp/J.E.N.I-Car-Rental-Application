5368f1832713d5ae5514dae17443286cc6d91a47
Johnathan Aguilar <johnathan.aguilar.350@my.csun.edu>
2024-10-01 02:25:16 -0700

    added a login for the admin

--- front_end/mainWindow.py
+++ front_end/mainWindow.py
@@ -4,6 +4,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from customerWindow import customer_window
 from adminWindow import admin_window
+from login import login
 
 class main_window(QMainWindow):
     def __init__(self):
@@ -12,6 +13,7 @@ class main_window(QMainWindow):
         self.setContentsMargins(0, 0, 0, 0)
         self.resize(1155, 912)
         self.setStyleSheet("background-color:rgb(255, 255, 255)")
+        self.setGeometry(150, 150, 1920, 1080)
 
         self.central_widget = QWidget(self)
         self.central_widget.setObjectName("central_widget")
@@ -26,8 +28,10 @@ class main_window(QMainWindow):
 
         self.c_window = customer_window()
         self.a_window = admin_window()
+        self.log_window = login()
         self.stacked_widget.addWidget(self.c_window)
         self.stacked_widget.addWidget(self.a_window)
+        self.stacked_widget.addWidget(self.log_window)
         self.stacked_widget.setCurrentIndex(0) 
 
         self.c_window.admin_button.clicked.connect(self.on_click_admin)
@@ -35,7 +39,14 @@ class main_window(QMainWindow):
 
 
     def on_click_admin(self):
-        self.stacked_widget.setCurrentIndex(1)
+        self.stacked_widget.setCurrentIndex(2)
+        self.log_window.login_button.clicked.connect(self.switch_to_admin)
+
+    def switch_to_admin(self):
+        self.pw = "password"
+        self.entered_pw = self.log_window.pw_box.text()
+        if self.pw == self.entered_pw: 
+            self.stacked_widget.setCurrentIndex(1)
 
     def on_click_customer(self):
         self.stacked_widget.setCurrentIndex(0)
