5532d4b188b9a2859e72a1c16a544260438548e3
Johnathan Aguilar <johnathan.aguilar.350@my.csun.edu>
2024-10-02 19:02:12 -0700

    finished refactoring admin and customer widget

--- front_end/adminWindow.py
+++ front_end/adminWindow.py
@@ -7,42 +7,53 @@ class admin_window(QWidget):
     def __init__(self):
         super().__init__()
         self.admin_layout = QVBoxLayout(self)
-        self.admin_layout.setContentsMargins(0, 0, 0, 0)
-        self.admin_layout.setSpacing(0)
-
-        #set up logo img
         self.logo = QLabel(self)
+        self.top_frame = QFrame(self)
         self.pixmap = QPixmap("logo/FullLogo.png")
+        self.top_frame_layout = QHBoxLayout(self.top_frame)
+        self.customer_button = QPushButton("Customer", self.top_frame)
+        self.bottom_frame = QFrame(self)
+
+        # Set up the font
+        self.set_font = font()
+        self.font = QFont(self.set_font.font_family, 16)
+        #self.font.setBold(True)
+
+        self.setup_logo()
+        self.setup_topbar()
+        self.setup_bottom()
+        self.setup_button()
+        self.setup_widget()
+
+    def setup_logo(self):#set up logo img
         self.logo.setPixmap(self.pixmap)
         self.logo.resize(self.pixmap.width(), self.pixmap.height())
         self.scaled_pixmap = self.pixmap.scaled(120, 120, aspectRatioMode=1)  # width, height
         self.logo.setPixmap(self.scaled_pixmap)
         self.logo.setStyleSheet("border: none;")
 
-        # Set up the font
-        self.set_font = font()
-        self.font = QFont(self.set_font.font_family, 16)
-        #self.font.setBold(True)
-        
-        self.top_frame = QFrame(self)
+    def setup_topbar(self):
         self.top_frame.setMaximumHeight(150)
         self.top_frame.setFrameShape(QFrame.StyledPanel)
         self.top_frame.setFrameShadow(QFrame.Raised)
         self.top_frame.setStyleSheet("background:white;")
-
-        self.top_frame_layout = QHBoxLayout(self.top_frame)
         self.top_frame_layout.addWidget(self.logo)
         self.top_frame_layout.addStretch()
-        self.customer_button = QPushButton("Customer", self.top_frame)
         self.top_frame_layout.addWidget(self.customer_button)
+        
+    def setup_bottom(self):
         self.customer_button.setFixedWidth(200)
         self.customer_button.setFixedHeight(75)
         self.customer_button.setFont(self.font)
         self.customer_button.setStyleSheet("background:white; border-radius: 5px; border: 3px solid lightgrey;")
 
-        self.bottom_frame = QFrame(self)
+
+    def setup_button(self):
         self.bottom_frame.setFrameShape(QFrame.StyledPanel)
         self.bottom_frame.setFrameShadow(QFrame.Raised)
 
+    def setup_widget(self):
+        self.admin_layout.setContentsMargins(0, 0, 0, 0)
+        self.admin_layout.setSpacing(0)
         self.admin_layout.addWidget(self.top_frame)
         self.admin_layout.addWidget(self.bottom_frame)
--- front_end/customerWindow.py
+++ front_end/customerWindow.py
@@ -7,51 +7,51 @@ from font import font
 class customer_window(QWidget):
     def __init__(self):
         super().__init__()
-
-        self.customer_layout = QVBoxLayout(self)
-        self.customer_layout.setContentsMargins(0, 0, 0, 0)
-        self.customer_layout.setSpacing(0)
-
-        self.setup_font()
-        self.setup_logo()
-
-        self.top_frame = QFrame(self)
-        self.top_frame.setFrameShape(QFrame.StyledPanel)
-        self.top_frame.setFixedHeight(150)
-        self.top_frame.setStyleSheet("background: white;")
-
-        self.top_frame_layout = QHBoxLayout(self.top_frame)
-        self.top_frame_layout.addWidget(self.logo)
-        self.top_frame_layout.addStretch()
-
-        self.admin_button = QPushButton("Admin",self.top_frame)
-        self.admin_button.setFixedWidth(200)
-        self.admin_button.setFixedHeight(75)
-        self.admin_button.setFont(self.font)
-        self.admin_button.setStyleSheet("background:white; border-radius: 5px; border: 3px solid lightgrey;")
-
-
-        self.bottom_frame = QFrame(self)
-        self.bottom_frame.setFrameShape(QFrame.StyledPanel)
-        self.bottom_frame.setFrameShadow(QFrame.Raised)
-    
-        self.top_frame_layout.addWidget(self.admin_button)
-        self.customer_layout.addWidget(self.top_frame)
-        self.customer_layout.addWidget(self.bottom_frame)
-
-    def setup_logo(self):#set up logo img
         self.logo = QLabel(self)
         self.pixmap = QPixmap("logo/FullLogo.png")
+        self.customer_layout = QVBoxLayout(self)
+        self.top_frame = QFrame(self)
+        self.top_frame_layout = QHBoxLayout(self.top_frame)
+        self.admin_button = QPushButton("Admin",self.top_frame)
+        self.bottom_frame = QFrame(self)
+        self.set_font = font()
+        self.font = QFont(self.set_font.font_family, 16)
+
+        self.setup_logo()
+        self.setup_topframe()
+        self.setup_bottomframe()
+        self.setup_button()
+        self.setup_widget()
+
+    def setup_logo(self):#set up logo img
         self.logo.setPixmap(self.pixmap)
         self.logo.resize(self.pixmap.width(), self.pixmap.height())
         self.scaled_pixmap = self.pixmap.scaled(120, 120, aspectRatioMode=1)  # width, height
         self.logo.setPixmap(self.scaled_pixmap)
         self.logo.setStyleSheet("border: none;")
 
-    def setup_font(self):# Set up the font
-        self.set_font = font()
-        self.font = QFont(self.set_font.font_family, 16)
-        #self.font.setBold(True)
+    def setup_topframe(self):
+        self.top_frame.setFrameShape(QFrame.StyledPanel)
+        self.top_frame.setFixedHeight(150)
+        self.top_frame.setStyleSheet("background: white;")
+        self.top_frame_layout.addWidget(self.logo)
+        self.top_frame_layout.addStretch()
 
+    def setup_button(self):
+        self.admin_button.setFixedWidth(200)
+        self.admin_button.setFixedHeight(75)
+        self.admin_button.setFont(self.font)
+        self.admin_button.setStyleSheet("background:white; border-radius: 5px; border: 3px solid lightgrey;")
+
+    def setup_bottomframe(self):
+        self.bottom_frame.setFrameShape(QFrame.StyledPanel)
+        self.bottom_frame.setFrameShadow(QFrame.Raised)
+ 
+    def setup_widget(self):
+        self.customer_layout.setContentsMargins(0, 0, 0, 0)
+        self.customer_layout.setSpacing(0)
+        self.top_frame_layout.addWidget(self.admin_button)
+        self.customer_layout.addWidget(self.top_frame)
+        self.customer_layout.addWidget(self.bottom_frame)
 
 
