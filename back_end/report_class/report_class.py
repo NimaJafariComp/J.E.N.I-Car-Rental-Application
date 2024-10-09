b16415359c6b85cbd0d4c7b5dd645cd2b40b4dca
Elijah Sagaran <elijah.sagaran.978@my.csun.edu>
2024-10-09 08:54:30 -0700

    fixed report_class.py to adhere to new structure

--- back_end/report_class/report_class.py
+++ back_end/report_class/report_class.py
@@ -1,30 +1,23 @@
 class report:
-    def __init__(self, customer_id, car_id, damage, mileage, gas_amount):
-       self.customer_id = customer_id
-       self.car_id = car_id
-       self.damage = damage
-       self.mileage = mileage
-       self.gas_amount = gas_amount
-       self.report_id = None
-        
-    def get_customer_id(self):
-       return self.customer_id
-        
-    def set_customer_id(self, customer_id):
-       self.customer_id = customer_id
+    def __init__(self, damages, gas_amount, car_id, reservation_id):
+        self.report_id = None
+        self.damages = damage
+        self.gas_amount = gas_amount
+        self.car_id = car_id
+        self.reservation_id = reservation_id
         
     def get_car_id(self):
        return self.car_id
         
     def get_damage(self):
        return self.damage
-        
-    def get_mileage(self):
-       return self.mileage
-        
+            
     def get_gas_amount(self):
        return self.gas_amount
-        
+    
+    def get_reservation_id(self):
+        return self.reservation_id
+    
     def get_report_id(self):
        return self.report_id
         
