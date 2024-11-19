7cecf44d62de2041b861009e87468c9a1adb2f57
Johnathan Aguilar <johnathan.aguilar.350@my.csun.edu>
2024-11-18 16:18:56 -0800

    put the tunnel in a modeuel

--- main.py
+++ main.py
@@ -1,36 +1,12 @@
 import sys
-import os
 from PyQt5.QtWidgets import *
 from front_end.config.screenConfig import screen_config
 from front_end.mainWindow import main_window
-from sshtunnel import SSHTunnelForwarder
-
+from tunnel import tunnel
 
 def main():
-    screenConfig = screen_config()
-    # SSH and Database Configurations
-    ec2_host = "13.52.253.115"  # EC2 instance public IP or hostname
-    ssh_username = "ubuntu"        # The SSH username for your EC2 instance (usually 'ec2-user' or 'ubuntu')
-    ssh_private_key_path = os.path.join(os.path.dirname(__file__), "TunnelKey.pem")  # Path to your SSH private key
-    rds_host = "jeni2.cfeouw8igyj4.us-west-1.rds.amazonaws.com"  # The RDS endpoint
-    rds_port = 3306  # Typically, this is the default MySQL port, adjust if you're using a different DBMS
-    
-    # Create SSH Tunnel through EC2 to access the RDS
-    with SSHTunnelForwarder(
-        (ec2_host, 22),  # EC2 public IP and SSH port
-        ssh_username=ssh_username,
-        ssh_pkey=ssh_private_key_path,
-        remote_bind_address=(rds_host, rds_port),  # RDS endpoint and port
-        local_bind_address=('127.0.0.1', 3307)  # You can set any available local port
-    ) as tunnel:
-        print(f"Tunnel established at 127.0.0.1:3307")
-        
-        try:
-            while True:
-                run()  # Run your application logic here
-        except KeyboardInterrupt:
-            print("\nTerminating the tunnel...")
-    
+    tunnel(run)
+
 def run():
     screenConfig = screen_config()
     app = QApplication(sys.argv)
