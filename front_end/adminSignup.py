be7572435a6b2d5f0e0033002023a0b989ee9106
Johnathan Aguilar <johnathan.aguilar.350@my.csun.edu>
2024-12-04 00:45:49 -0800

    stated to make the sign up for the customer side work with the back end

--- front_end/adminSignup.py
+++ front_end/adminSignup.py
@@ -1,19 +1,23 @@
 import os
-from PyQt5.QtWidgets import *
+
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-from .config.font import font
+from PyQt5.QtWidgets import *
+
 from .api import api
+from .config.font import font
 from .currentUser import CurrentUser
 
+
 class admin_signup(QWidget):
-    '''
+    """
     Widget to make the login window.
-    '''
+    """
+
     def __init__(self, main_window):
-        '''
+        """
         Initializes the login window.
-        '''
+        """
         super().__init__()
         self.main_window = main_window
         self.api = api()
@@ -23,31 +27,31 @@ class admin_signup(QWidget):
         self.login_window = QWidget()
         self.login_window_layout = QVBoxLayout(self.login_window)
 
-        #set up logo img
+        # set up logo img
         self.logo = QLabel()
         self.logo_dir = os.path.join(os.path.dirname(__file__), "logo/HalfLogo.png")
         self.pixmap = QPixmap(self.logo_dir)
 
-
         # Set up the font
         self.set_font = font()
         self.font = QFont(self.set_font.font_family, 16)
-        #self.font.setBold(True)
+        # self.font.setBold(True)
 
-        # Create label and set font 
-        self.label_style = "background-color:white; border : 1px solid lightgrey; border-radius : 5px;" 
+        # Create label and set font
+        self.label_style = (
+            "background-color:white; border : 1px solid lightgrey; border-radius : 5px;"
+        )
         self.admin_label = QLabel("Admin Sign Up")
         self.pw_not_same = QLabel("Passwords do not match try again.")
         self.pw_not_same.setStyleSheet("border : none; color : red;")
         self.pw_not_same.hide()
         self.first_name_box = QLineEdit()
         self.last_name_box = QLineEdit()
-        self.dob_box = QDateEdit()
         self.user_box = QLineEdit()
         self.email = QLineEdit()
         self.pw_box = QLineEdit()
         self.pw_confirm_box = QLineEdit()
-        
+
         # Login button
         self.buttons = QFrame()
         self.buttons_layout = QHBoxLayout(self.buttons)
@@ -61,22 +65,24 @@ class admin_signup(QWidget):
         self.setup_logo()
         self.setup_admin_label()
         self.text_box()
-        self.setup_login_button() 
+        self.setup_login_button()
 
     def setup_logo(self):
-        '''
-        function to set parameters for the image logo for the login window.        
-        '''
+        """
+        function to set parameters for the image logo for the login window.
+        """
         self.logo.setPixmap(self.pixmap)
         self.logo.resize(self.pixmap.width(), self.pixmap.height())
-        self.scaled_pixmap = self.pixmap.scaled(300, 300, aspectRatioMode=1)  # width, height
+        self.scaled_pixmap = self.pixmap.scaled(
+            300, 300, aspectRatioMode=1
+        )  # width, height
         self.logo.setPixmap(self.scaled_pixmap)
         self.logo.setStyleSheet("border: none;")
 
     def setup_layout(self):
-        '''
+        """
         function to setup the main layou for the login window.
-        '''
+        """
         self.setStyleSheet("background-color:darkgrey;")
         self.login_layout.setContentsMargins(0, 0, 0, 0)
         self.login_layout.setSpacing(0)
@@ -98,32 +104,33 @@ class admin_signup(QWidget):
         self.login_window_layout.addStretch()
         self.login_window_layout.addWidget(self.admin_label, alignment=Qt.AlignCenter)
         self.login_window_layout.addWidget(self.pw_not_same, alignment=Qt.AlignCenter)
-        self.login_window_layout.addWidget(self.first_name_box, alignment=Qt.AlignCenter)
+        self.login_window_layout.addWidget(
+            self.first_name_box, alignment=Qt.AlignCenter
+        )
         self.login_window_layout.addWidget(self.last_name_box, alignment=Qt.AlignCenter)
-        self.login_window_layout.addWidget(self.dob_box, alignment=Qt.AlignCenter)
         self.login_window_layout.addWidget(self.user_box, alignment=Qt.AlignCenter)
         self.login_window_layout.addWidget(self.email, alignment=Qt.AlignCenter)
         self.login_window_layout.addWidget(self.pw_box, alignment=Qt.AlignCenter)
-        self.login_window_layout.addWidget(self.pw_confirm_box, alignment=Qt.AlignCenter)
+        self.login_window_layout.addWidget(
+            self.pw_confirm_box, alignment=Qt.AlignCenter
+        )
         self.login_window_layout.addWidget(self.buttons, alignment=Qt.AlignCenter)
         self.login_window_layout.addStretch()
 
         # Add the login window to the main layout, centering it
         self.login_layout.addWidget(self.login_window, alignment=Qt.AlignCenter)
 
-
-
     def setup_admin_label(self):
-        '''
+        """
         funtion to set up the parameters for the admin label.
-        '''
+        """
         self.admin_label.setFont(self.font)
         self.admin_label.setStyleSheet("color: #efbe25; border: none;")
 
     def text_box(self):
-        '''
+        """
         function to set the parameters for the user name text box.
-        '''
+        """
         self.first_name_box.setPlaceholderText("First Name")
         self.first_name_box.setFixedWidth(300)
         self.first_name_box.setFixedHeight(40)
@@ -138,12 +145,6 @@ class admin_signup(QWidget):
         self.email.setFixedWidth(300)
         self.email.setFixedHeight(40)
         self.email.setStyleSheet(self.label_style)
-
-        self.dob_box.setFixedWidth(300)
-        self.dob_box.setFixedHeight(40)
-        self.dob_box.setDisplayFormat("MM/dd/yyyy")
-        self.dob_box.setButtonSymbols(QAbstractSpinBox.NoButtons)
-        self.dob_box.setStyleSheet(self.label_style)
 
         self.user_box.setPlaceholderText("Enter Username")
         self.user_box.setFixedWidth(300)
@@ -163,9 +164,9 @@ class admin_signup(QWidget):
         self.pw_confirm_box.setStyleSheet(self.label_style)
 
     def setup_login_button(self):
-        '''
+        """
         function to set up the parameters for the login button.
-        '''
+        """
         self.login_button.setFont(self.font)
         self.login_button.setFixedWidth(125)
         self.login_button.setFixedHeight(40)
@@ -180,19 +181,20 @@ class admin_signup(QWidget):
         self.buttons_layout.addWidget(self.login_button)
         self.login_button.clicked.connect(self.click_signup)
         self.back_button.clicked.connect(self.click_back)
-        
 
     def click_signup(self):
-        '''
+        """
         funtion to check login information and login to admin side when login button in login window is pressed.
-        '''
+        """
         if self.pw_box.text() == self.pw_confirm_box.text():
             name = self.first_name_box.text() + " " + self.last_name_box.text()
             username = self.user_box.text()
             email = self.email.text()
             pw = self.pw_box.text()
-            dob = self.dob_box.date() 
-            self.api.car_rental_obj.user_signup(name, username, email, pw, dob.toString("MM/dd/yyyy"), "admin")
+            dob = None
+            self.api.car_rental_obj.user_signup(
+                name, username, email, pw, dob.toString("MM/dd/yyyy"), "admin"
+            )
             self.main_window.stacked_widget.setCurrentIndex(0)
         else:
             self.pw_not_same.show()
--- front_end/customerSignup.py
+++ front_end/customerSignup.py
@@ -1,17 +1,21 @@
 import os
-from PyQt5.QtWidgets import *
+
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
 from .config.font import font
 
+
 class customer_signup(QWidget):
-    '''
+    """
     Widget to make the login window.
-    '''
+    """
+
     def __init__(self, main_window):
-        '''
+        """
         Initializes the login window.
-        '''
+        """
         super().__init__()
         self.main_window = main_window
 
@@ -20,26 +24,30 @@ class customer_signup(QWidget):
         self.login_window = QWidget()
         self.login_window_layout = QVBoxLayout(self.login_window)
 
-        #set up logo img
+        # set up logo img
         self.logo = QLabel()
         self.logo_dir = os.path.join(os.path.dirname(__file__), "logo/HalfLogo.png")
         self.pixmap = QPixmap(self.logo_dir)
 
-
         # Set up the font
         self.set_font = font()
         self.font = QFont(self.set_font.font_family, 16)
-        #self.font.setBold(True)
+        # self.font.setBold(True)
 
-        # Create label and set font 
-        self.label_style = "background-color:white; border : 1px solid lightgrey; border-radius : 5px;" 
+        # Create label and set font
+        self.label_style = (
+            "background-color:white; border : 1px solid lightgrey; border-radius : 5px;"
+        )
         self.admin_label = QLabel("Customer Sign Up")
+        self.pw_not_same = QLabel("Passwords do not match try again.")
+        self.pw_not_same.setStyleSheet("border : none; color : red;")
+        self.pw_not_same.hide()
         self.first_name_box = QLineEdit()
         self.last_name_box = QLineEdit()
         self.user_box = QLineEdit()
         self.email = QLineEdit()
         self.pw_box = QLineEdit()
-        
+
         # Login button
         self.buttons = QFrame()
         self.buttons_layout = QHBoxLayout(self.buttons)
@@ -53,22 +61,24 @@ class customer_signup(QWidget):
         self.setup_logo()
         self.setup_admin_label()
         self.text_box()
-        self.setup_login_button() 
+        self.setup_login_button()
 
     def setup_logo(self):
-        '''
-        function to set parameters for the image logo for the login window.        
-        '''
+        """
+        function to set parameters for the image logo for the login window.
+        """
         self.logo.setPixmap(self.pixmap)
         self.logo.resize(self.pixmap.width(), self.pixmap.height())
-        self.scaled_pixmap = self.pixmap.scaled(300, 300, aspectRatioMode=1)  # width, height
+        self.scaled_pixmap = self.pixmap.scaled(
+            300, 300, aspectRatioMode=1
+        )  # width, height
         self.logo.setPixmap(self.scaled_pixmap)
         self.logo.setStyleSheet("border: none;")
 
     def setup_layout(self):
-        '''
+        """
         function to setup the main layou for the login window.
-        '''
+        """
         self.setStyleSheet("background-color:darkgrey;")
         self.login_layout.setContentsMargins(0, 0, 0, 0)
         self.login_layout.setSpacing(0)
@@ -89,7 +99,9 @@ class customer_signup(QWidget):
         self.login_window_layout.addWidget(self.logo, alignment=Qt.AlignCenter)
         self.login_window_layout.addStretch()
         self.login_window_layout.addWidget(self.admin_label, alignment=Qt.AlignCenter)
-        self.login_window_layout.addWidget(self.first_name_box, alignment=Qt.AlignCenter)
+        self.login_window_layout.addWidget(
+            self.first_name_box, alignment=Qt.AlignCenter
+        )
         self.login_window_layout.addWidget(self.last_name_box, alignment=Qt.AlignCenter)
         self.login_window_layout.addWidget(self.user_box, alignment=Qt.AlignCenter)
         self.login_window_layout.addWidget(self.email, alignment=Qt.AlignCenter)
@@ -100,19 +112,17 @@ class customer_signup(QWidget):
         # Add the login window to the main layout, centering it
         self.login_layout.addWidget(self.login_window, alignment=Qt.AlignCenter)
 
-
-
     def setup_admin_label(self):
-        '''
+        """
         funtion to set up the parameters for the admin label.
-        '''
+        """
         self.admin_label.setFont(self.font)
         self.admin_label.setStyleSheet("color: #efbe25; border: none;")
 
     def text_box(self):
-        '''
+        """
         function to set the parameters for the user name text box.
-        '''
+        """
         self.first_name_box.setPlaceholderText("First Name")
         self.first_name_box.setFixedWidth(300)
         self.first_name_box.setFixedHeight(40)
@@ -140,9 +150,9 @@ class customer_signup(QWidget):
         self.pw_box.setStyleSheet(self.label_style)
 
     def setup_login_button(self):
-        '''
+        """
         function to set up the parameters for the login button.
-        '''
+        """
         self.login_button.setFont(self.font)
         self.login_button.setFixedWidth(125)
         self.login_button.setFixedHeight(40)
@@ -155,16 +165,29 @@ class customer_signup(QWidget):
 
         self.buttons_layout.addWidget(self.back_button)
         self.buttons_layout.addWidget(self.login_button)
-        self.login_button.clicked.connect(self.click_login)
+        self.login_button.clicked.connect(self.click_signup)
         self.back_button.clicked.connect(self.click_back)
-        
 
-    def click_login(self):
-        '''
+    def click_signup(self):
+        """
         funtion to check login information and login to admin side when login button in login window is pressed.
-        '''
-        self.main_window.stacked_widget.setCurrentIndex(0)
+        """
+        if self.pw_box.text() == self.pw_confirm_box.text():
+            name = self.first_name_box.text() + " " + self.last_name_box.text()
+            username = self.user_box.text()
+            email = self.email.text()
+            pw = self.pw_box.text()
+            dob = None
+            self.api.car_rental_obj.user_signup(
+                name, username, email, pw, dob.toString("MM/dd/yyyy"), "admin"
+            )
+            self.main_window.stacked_widget.setCurrentIndex(0)
+        else:
+            self.pw_not_same.show()
+            return
+
         self.pw_box.clear()
+        self.pw_confirm_box.clear()
 
     def click_back(self):
         self.main_window.stacked_widget.setCurrentIndex(2)
